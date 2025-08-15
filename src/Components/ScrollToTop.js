599cefbbccbd846581dffc9b8f8542c1825321c0
Anurag Sharma <anuragwork2005@gmail.com>
2025-08-15 08:20:37 +0530

    re update the scrolling code

--- src/Components/ScrollToTop.js
+++ src/Components/ScrollToTop.js
@@ -1,13 +1,15 @@
-// src/ScrollToTop.jsx
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+// ScrollToTop.js
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-export default function ScrollToTop() {
+const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0); // Page change hote hi top pe scroll
+    window.scrollTo({ top: 0, behavior: 'auto' }); 
   }, [pathname]);
 
   return null;
-}
+};
+
+export default ScrollToTop;
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   base: '/',
-
+  server: {
+    host: '0.0.0.0', // phone se access karne ke liye
+    port: 5173       // optional: custom port rakhna ho to
+  }
 })
